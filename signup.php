2ff1b786aec95475ce42c64cc6fde56ad746d72d
rashmiFartode <fartoderashmi1@gmail.com>
2019-06-25 12:25:04 +0530

    first commit

---
+++ signup.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Sign up</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="keywords" content="Sign Up Form Responsive Widget, Audio and Video players, Login Form Web Template, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design">
+	<script>
+        addEventListener("load", function () { setTimeout(hideURLbar, 0); }, false); function hideURLbar() { window.scrollTo(0, 1); }
+    </script>
+    <!-- ends meta tag -->
+    <!-- font-awesome icons -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- //font-awesome icons -->
+    <!--stylesheets-->
+    <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+    <!--//style sheet end here-->
+    <link href="//fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
+</head>
+<body>
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<h1 class="heading"> Welcome to admin panel</h1>
+				</div>
+				<div class="col-md-7 float-md-left p-3">
+					<h3>MANAGE YOUR GADGETS ACCOUNT</h3>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula ege</p>
+					<img src="images/b11.png" class="img-fluid" alt="background">
+				</div>
+				<div class="col-md-5">
+					<div class="card">
+						<div class="card-header">
+							Sign up here
+						</div>
+						<div class="card-body">
+							<form>
+								<div class="form-group">
+									<input type="text" name="">
+								</div>
+								<div class="form-group">
+									<input type="number" name="">
+								</div>
+								<div class="form-group">
+									<input type="email" name="">
+								</div>
+								<div class="form-group">
+									<input type="password" name="">
+								</div>
+								<div class="row">
+									<div class="col-md-6">
+										<input type="checkbox" name="">Remember me
+									</div>
+									<div class="col-md-6">
+										<a href="" class="card-link">Forget password?</a>
+									</div>
+								</div>
+								<input type="Submit" name="signup" value="Sign up" class="btn btn-success d-block"> 
+							</form>
+							<div class="col-12">
+								<h3>Already have account?</h3>
+								<a href="" class="card-link">Sign In here</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<p>© 2019 Sign Up Form. All Rights Reserved | Design by rashmi</p>
+	</footer>
+</body>
+</html>
